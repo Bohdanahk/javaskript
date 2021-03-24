8f8b690928155ea90b5c234b1c181365c8940564
New <new@News-MacBook.local>
2021-03-24 20:17:40 +0200

    new

--- javascript./carousel.js
+++ javascript./carousel.js
@@ -8,8 +8,14 @@ const carouselImages = [
 let currentIdx = 0;
 
 function showCurrentImage() {
-    const imgContainer = document.querySelector('.products-carousel .current-image');
-    imgContainer.src =  carouselImages[currentIdx];       
+    const imgContainer1 = document.querySelector('.products-carousel .image1');
+    const imgContainer2 = document.querySelector('.products-carousel .image2');
+    const imgContainer3 = document.querySelector('.products-carousel .image3');
+    const img2Idx = currentIdx + 1 > carouselImages.length - 1 ? 0 : currentIdx + 1;      
+    const img3Idx = img2Idx + 1 > carouselImages.length - 1 ? 0 : img2Idx + 1;      
+    imgContainer1.src =  carouselImages[currentIdx]; 
+    imgContainer2.src =  carouselImages[img2Idx]; 
+    imgContainer3.src =  carouselImages[img3Idx]; 
 }
 
 function next() {
@@ -31,5 +37,5 @@ setInterval(next, 2000);
 document.querySelector('.products-carousel .next')
   .addEventListener('click', next);
 
-  document.querySelector('.products-carousel .prev')
+document.querySelector('.products-carousel .prev')
   .addEventListener('click', prev);
